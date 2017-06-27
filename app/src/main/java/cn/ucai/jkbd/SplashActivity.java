1ec2ed787baabf0f072f71583046d8e89e929a60
耿小泰 <clawpo@foxmail.com>
2017-06-27 17:01:40 +0800

    闪屏2秒跳转到主页

--- app/src/main/java/cn/ucai/jkbd/SplashActivity.java
+++ app/src/main/java/cn/ucai/jkbd/SplashActivity.java
@@ -1,6 +1,8 @@
 package cn.ucai.jkbd;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.os.CountDownTimer;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 
@@ -13,5 +15,19 @@ public class SplashActivity extends AppCompatActivity {
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash);
+        mCountDownTimer.start();
     }
+
+    CountDownTimer mCountDownTimer  = new CountDownTimer(2000,1000) {
+        @Override
+        public void onTick(long millisUntilFinished) {
+
+        }
+
+        @Override
+        public void onFinish() {
+            Intent intent = new Intent(SplashActivity.this,MainActivity.class);
+            startActivity(intent);
+        }
+    };
 }
