ae746215aebd294e4aafc4e49132b04aac949e2b
耿小泰 <clawpo@foxmail.com>
2017-06-27 16:42:40 +0800

    添加闪屏的activity

---
+++ app/src/main/java/cn/ucai/jkbd/SplashActivity.java
@@ -0,0 +1,17 @@
+package cn.ucai.jkbd;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by clawpo on 2017/6/27.
+ */
+
+public class SplashActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+    }
+}
